aa3ab2d27b24e1ce6a9841e2ff0459bf01bf903f
unknown <mozhno_prost_kostya@mail.ru>
2026-03-10 00:52:43 +0300

    feat: move quiz widget to repo

--- src/pages/widget-engine-page.ts
+++ src/pages/widget-engine-page.ts
@@ -1,42 +1,143 @@
 import { BaseComponent, type Page } from '@/core';
 
-// Это можно будет вынести во внешний файл типа utils
-function createHeading(text: string): HTMLHeadingElement {
-  const element = document.createElement('h1');
-  element.textContent = text;
-  return element;
+import './style.css';
+// TYPES
+type quizeQuestion = {
+  id: string;
+  question: string;
+  answers: string[];
+  rightAnswer: number;
+};
+
+type QuizState = {
+  currentIndex: number;
+  selectedIndex: number | null;
+  isLocked: boolean;
+  score: number;
+};
+
+//DATA
+const questions: quizeQuestion[] = [
+  {
+    id: 'q1',
+    question: 'Что вернет typeof null?',
+    answers: ['null', 'undefined', 'object', 'NaN'],
+    rightAnswer: 2,
+  },
+  {
+    id: 'q2',
+    question: 'Какой метод добавляет элемент в конец массива?',
+    answers: ['push', 'pop', 'shift', 'map'],
+    rightAnswer: 0,
+  },
+  {
+    id: 'q3',
+    question: 'Что делает === ?',
+    answers: ['сравнение по типу', 'строгое сравнение', 'преобразует строку в число', 'проверяет null'],
+    rightAnswer: 1,
+  },
+  {
+    id: 'q4',
+    question: "Что вернет Boolean('')",
+    answers: ['false', 'true', 'undefined', 'error'],
+    rightAnswer: 0,
+  },
+  {
+    id: 'q5',
+    question: 'Какой тип у undefined?',
+    answers: ['null', 'object', 'boolean', 'underfinded'],
+    rightAnswer: 3,
+  },
+];
+
+const state: QuizState = {
+  currentIndex: 0,
+  selectedIndex: null,
+  isLocked: false,
+  score: 0,
+};
+
+// RENDERR
+
+function renderQuiz() {
+  const screen = document.createElement('div');
+  screen.classList.add('quiz-screen');
+
+  const questionBlock = document.createElement('div');
+  questionBlock.classList.add('question-block');
+  const currentQuestion = questions[state.currentIndex];
+
+  if (!currentQuestion) throw new Error('Нету вопроса');
+  questionBlock.textContent = currentQuestion.question;
+
+  const answersBlock = document.createElement('div');
+  answersBlock.classList.add('answers-block');
+
+  currentQuestion.answers.forEach((answerText, index) => {
+    const button = document.createElement('button');
+    button.classList.add('answer-btn');
+    button.type = 'button';
+    button.textContent = answerText;
+
+    button.addEventListener('click', () => choseAnswer(index));
+
+    if (state.isLocked) {
+      if (index === currentQuestion.rightAnswer) {
+        button.classList.add('correct');
+      }
+
+      if (state.selectedIndex === index && state.selectedIndex != currentQuestion.rightAnswer) {
+        button.classList.add('wrong');
+      }
+    }
+    answersBlock.append(button);
+  });
+
+  const moveBlock = document.createElement('div');
+  moveBlock.classList.add('move-block');
+
+  if (state.isLocked) {
+    const nextButton = document.createElement('button');
+    nextButton.classList.add('next-btn');
+    nextButton.textContent = 'Next';
+
+    nextButton.addEventListener('click', () => nextQuestion());
+    moveBlock.append(nextButton);
+  }
+
+  screen.append(questionBlock, answersBlock, moveBlock);
+
+  return screen;
 }
 
-function createButton(text: string, onClick: () => void): HTMLButtonElement {
-  const button = document.createElement('button');
-  button.textContent = text;
-  button.addEventListener('click', onClick);
-  return button;
+//FUNKSH
+
+function choseAnswer(index: number) {
+  if (state.isLocked) return;
+  state.selectedIndex = index;
+
+  const currentQuestion = questions[state.currentIndex];
+  if (!currentQuestion) throw new Error('Нету вопроса');
+
+  if (state.selectedIndex === currentQuestion.rightAnswer) {
+    state.score++;
+  }
+
+  state.isLocked = true;
 }
 
-function createContainer(className: string, ...children: HTMLElement[]): HTMLDivElement {
-  const div = document.createElement('div');
-  div.className = className;
-  children.forEach((child) => div.append(child));
-  return div;
-}
+function nextQuestion() {
+  state.currentIndex++;
 
-// Это уже для самого DOM
-function handleClick(selector: string) {
-  const h1 = document.querySelector(selector);
-  if (h1) h1.textContent = 'Текст изменён после клика!';
+  state.selectedIndex = null;
+  state.isLocked = false;
 }
 
 export function widgetEnginePage(): Page {
   return {
     render() {
-      // Создаём DOM
-      const h1 = createHeading('Привет из нативного DOM');
-      const button = createButton('Нажми меня', () => handleClick('h1'));
-      const container = createContainer('my-page', h1, button);
-
       //в конце оборачиваем всё в BaseComponent (для совместимости)
-      return new BaseComponent(container);
+      return new BaseComponent(renderQuiz());
     },
     onMount: () => console.log('Страница смонтирована'),
   };
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vitest/config';
 import eslint from 'vite-plugin-eslint2';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [
-    eslint({ overrideConfigFile: './eslint.config.js' })
-  ],
+  plugins: [eslint({ overrideConfigFile: './eslint.config.js' })],
   resolve: {
     alias: {
-      '@': new URL('src', import.meta.url).pathname,
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
